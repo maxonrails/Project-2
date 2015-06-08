3a667f2c6cba220cb7faa84d2be5b7b0a08dc00f
Yo Wakita <wakita@gmail.com>
2015-06-07 21:07:48 -0700

    Deleted test model and controller

---
+++ db/migrate/20150608035555_drop_tests_table.rb
@@ -0,0 +1,4 @@
+class DropTestsTable < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607212320) do
+ActiveRecord::Schema.define(version: 20150608035555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
